2fd3e4036e5f506aa5a66118a5bd803875a5a04c
idosinchuk <idosinchuk@host.docker.internal>
2019-09-19 11:32:23 +0200

    Adding project

---
+++ src/main/java/com/idosinchuk/javaeleven/PreparationCertification1Z0815Application.java
@@ -0,0 +1,13 @@
+package com.idosinchuk.javaeleven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparationCertification1Z0815Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreparationCertification1Z0815Application.class, args);
+	}
+
+}
